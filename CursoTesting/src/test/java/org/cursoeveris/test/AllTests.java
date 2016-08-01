173154ee94d767578e336d8ace1271669f1170aa
Lobo83 <inmortalland83@gmail.com>
2016-08-01 21:49:16 +0200

    pruebas curso

---
+++ CursoTesting/src/test/java/org/cursoeveris/test/AllTests.java
@@ -0,0 +1,12 @@
+package org.cursoeveris.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({Test1.class,
+	Test2.class})
+public class AllTests {
+
+}
